c12caa5578c8bac1e3d1eff3ad41132a7bdc55f5
Gamer2012 <65019724+YetAnotherCoder00@users.noreply.github.com>
2024-06-25 15:13:37 +0200

    setting up for github pages

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -3,6 +3,47 @@
   import Scene from './Scene.svelte'
 </script>
 
-<Canvas>
-  <Scene />
-</Canvas>
+<style>
+    * {
+        color: white;
+        font-family: monospace;
+    }
+    .body {
+        min-height: 100%;
+        padding-left: 12.5%;
+        padding-right: 12.5%;
+        display: flex;
+        align-content: center;
+        flex-direction: column;
+    }
+
+    .skills {
+        flex-grow: 2;
+        min-height: 65%;
+        border: rgb(200, 200, 200) 1px solid;
+        border-radius: 1vh;
+    }
+    
+    h1 {
+        font-size: 5vw;
+        text-align: center;
+    }
+
+    h2 {
+        color: rgb(200, 200, 200);
+        font-size: 3.5vw;
+        text-align: center;
+    }
+</style>
+<div class="body">
+    <h1> Leonardo Kreienbühl </h1>
+
+    <div class="kurzprofil"> </div>
+
+    <h2>Informatikkompetenzen</h2>
+    <div class="skills">
+        <Canvas>
+          <Scene />
+        </Canvas>
+    </div>
+</div>
