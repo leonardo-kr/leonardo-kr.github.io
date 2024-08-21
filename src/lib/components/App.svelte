e09cf175d266614202dfc770d79efc5df7ef3cb5
Leonardo <leonardo@kreienbuehl.net>
2024-08-21 22:36:32 +0200

    merged 'portfolioDev' into this repo

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,14 +1,108 @@
 <script lang="ts">
-  import { Canvas } from '@threlte/core';
-  import Scene from './Scene.svelte';
+    import { Canvas } from "@threlte/core";
+    import Scene from "./Scene.svelte";
 
-  import bugtracker from "$lib/assets/images/bugtracker.png";
-  import portfolioScreenshot from "$lib/assets/images/portfolioScreenshot.png";
+    import bugtracker from "$lib/assets/images/bugtracker.png";
+    import portfolioScreenshot from "$lib/assets/images/portfolioScreenshot.png";
+    import instantMessenger from "$lib/assets/images/instantMessenger.png";
 
-  import lebenslauf from "$lib/assets/docs/lebenslauf.pdf";
-  import modulnoten from "$lib/assets/docs/modulnoten.pdf";
-
+    import lebenslauf from "$lib/assets/docs/lebenslauf.pdf";
+    import modulnoten from "$lib/assets/docs/modulnoten.pdf";
 </script>
+
+<div class="body">
+    <h1>Leonardo Kreienbühl</h1>
+
+    <div class="kurzprofil">
+        Ich bin ein passionierter Informatiker und baue gerne Software. <br />
+        Am Liebsten programmiere ich mit C#, da ich damit am effizientesten arbeiten
+        kann, unabhängig davon, woran ich arbeiten muss. <br />
+        Mein Ziel ist es, eine Software zu kreieren, welche anderen Menschen hilft,
+        sei es ein kleines Tool für Informatiker/-innen oder ein grösseres Programm
+        für Benutzer/-innen. <br />
+    </div>
+
+    <h2>Informatikkompetenzen</h2>
+    <p>
+        Die im folgenden dargestellten vier Würfel zeigen verschiedene Aspekte
+        meiner Informatikkompetenzen. Je höher die Türme, desto grösser ist mein
+        Wissen und Können in der entsprechenden Sprache bzw. im entsprechenden
+        Bereich.
+    </p>
+    <i style="font-size: 0.9vw; color: rgb(180, 180, 180); padding-bottom: 1vh;"
+        >Mit der linken Maustaste können Sie die Kamera bewegen. Wenn Sie mit
+        der Maus über den Würfel gehen, werden Ihnen die Sprachen bzw. Bereiche
+        angezeigt mit Text.</i
+    >
+    <div class="skills">
+        <Canvas>
+            <Scene />
+        </Canvas>
+    </div>
+
+    <h2>Arbeitsbeispiele</h2>
+    <div class="examples">
+        <div>
+            <h3>Portfolio</h3>
+            <p>
+                Das erste Beispiel ist das Portfolio, in welchem Sie sich
+                bereits befinden. Den Quellcode können Sie auf
+                <a
+                    href="https://www.github.com/yetanothercoder00/yetanothercoder00.github.io"
+                    >GitHub</a
+                > anschauen.
+            </p>
+            <img src={portfolioScreenshot} alt="Screenshot vom Portfolio" />
+        </div>
+
+        <div>
+            <h3>Bug Tracker</h3>
+            <p>
+                Das zweite Beispiel ist ein "Bug Tracker", welchen ich mit
+                ASP.NET, C# und Microsoft SQL Server gebaut habe. Den Quellcode
+                dafür können Sie auch auf <a
+                    href="https://www.github.com/yetanothercoder00/bugtracker"
+                    >GitHub</a
+                > finden. Diese Webseite wurde mit dem MVC-Pattern und Entity Framework
+                Core erstellt.
+            </p>
+            <img src={bugtracker} alt="Screenshot vom Bug Tracker" />
+        </div>
+        <div>
+            <h3>Instant Messenger</h3>
+            <p>
+                Das dritte Beispiel ist ein "Instant Messenger", welcher
+                mithilfe von <a href="https://kit.svelte.dev">SvelteKit</a>
+                gebaut wurde. Hier werden Nachrichten in einer SQLite3 Datenbank
+                auf einem Server gespeichert und in Intervallen von einer
+                Sekunde abgerufen und angezeigt. Den Quellcode können Sie auf
+                <a
+                    href="https://www.github.com/yetanothercoder00/instantmessenger"
+                    >GitHub</a
+                > finden.
+            </p>
+            <img
+                src={instantMessenger}
+                alt="Screenshot des instant messengers"
+            />
+        </div>
+    </div>
+
+    <h2>Dokumente</h2>
+    <div class="docs">
+        <div>
+            <h3>Lebenslauf</h3>
+            <embed src={lebenslauf} type="application/pdf" />
+        </div>
+
+        <div>
+            <h3>Modulnotenübersicht</h3>
+            <embed src={modulnoten} type="application/pdf" />
+        </div>
+    </div>
+</div>
+
+<footer></footer>
 
 <style>
     * {
@@ -29,7 +123,7 @@
         border: white 4px solid;
         border-radius: 2px;
     }
-    
+
     h1 {
         padding-bottom: 2vh;
         font-size: 2vw;
@@ -64,7 +158,7 @@
         flex-direction: column;
         gap: 1vw;
     }
-    
+
     .examples div {
         display: flex;
         flex-direction: column;
@@ -97,64 +191,3 @@
         border: rgb(160, 160, 160) 4px solid;
     }
 </style>
-<div class="body">
-    <h1> Leonardo Kreienbühl </h1>
-
-    <div class="kurzprofil"> 
-        
-        Ich bin ein passionierter Informatiker und baue gerne Software. <br>
-        Am Liebsten programmiere ich mit C#, da ich damit am effizientesten arbeiten kann, unabhängig davon, woran ich arbeiten muss. <br>
-        Mein Ziel ist es, eine Software zu kreieren, welche anderen Menschen hilft, sei es ein kleines Tool für Informatiker/-innen oder ein grösseres Programm für Benutzer/-innen. <br>
-        
-
-    </div>
-
-    <h2>Informatikkompetenzen</h2>
-        <p>
-            Die im folgenden dargestellten vier Würfel zeigen verschiedene Aspekte meiner Informatikkompetenzen. Je höher die Türme, desto grösser ist mein Wissen und Können in der entsprechenden Sprache bzw. im entsprechenden Bereich. 
-        </p>
-        <i style="font-size: 0.9vw; color: rgb(180, 180, 180); padding-bottom: 1vh;">Mit der linken Maustaste können Sie die Kamera bewegen. Wenn Sie mit der Maus über den Würfel gehen, werden Ihnen die Sprachen bzw. Bereiche angezeigt mit Text.</i>
-    <div class="skills">
-        <Canvas>
-          <Scene />
-        </Canvas>
-    </div>
-
-    <h2>Arbeitsbeispiele</h2>
-    <div class="examples">
-        <div>
-            <h3>Portfolio</h3>
-            <p>
-                Das erste Beispiel ist das Portfolio, in welchem Sie sich bereits befinden. Den Quellcode können Sie auf
-                <a href="https://www.github.com/yetanothercoder00/yetanothercoder00.github.io">Github</a> anschauen.
-            </p>
-            <img src={portfolioScreenshot} alt="Screenshot vom Portfolio" />
-        </div>
-
-        <div>
-            <h3>Bug Tracker</h3>
-            <p>
-                Das zweite Beispiel ist ein "Bug Tracker", welchen ich mit ASP.NET, C# und Microsoft SQL Server gebaut habe. Den Quellcode dafür können Sie auch auf <a href="https://www.github.com/yetanothercoder00/bugtracker">Github</a> finden. Diese Webseite wurde mit dem MVC-Pattern und Entity Framework Core erstellt.
-            </p>
-            <img src={bugtracker} alt="Screenshot vom Bug Tracker" />
-        </div>
-    </div>
-
-    <h2>Dokumente</h2>
-    <div class="docs">
-        <div>
-            <h3>Lebenslauf</h3>
-            <embed src={lebenslauf} type="application/pdf">
-        </div>
-
-        <div>
-            <h3>Modulnotenübersicht</h3>
-            <embed src={modulnoten} type="application/pdf">
-        </div>
-    </div>
-    
-</div>
-
-<footer>
-
-</footer>
--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { T, useFrame } from '@threlte/core';
+    import { T, useFrame } from "@threlte/core";
     import { interactivity, OrbitControls } from "@threlte/extras";
     import { Vector3 } from "three";
 
@@ -18,12 +18,16 @@
     }
 
     interactivity();
-    
-    const backendPosition = {x: spring(-0.5), y: spring(0.5), z: spring(-0.5)};
-    const frontendPosition = {x: spring(20), y: spring(0.5), z: spring(-0.5)};
-    const databasePosition = {x: spring(40), y: spring(0.5), z: spring(-0.5)};
-    const toolPosition = {x: spring(60), y: spring(0.5), z: spring(-0.5)};
-    
+
+    const backendPosition = {
+        x: spring(-0.5),
+        y: spring(0.5),
+        z: spring(-0.5),
+    };
+    const frontendPosition = { x: spring(20), y: spring(0.5), z: spring(-0.5) };
+    const databasePosition = { x: spring(40), y: spring(0.5), z: spring(-0.5) };
+    const toolPosition = { x: spring(60), y: spring(0.5), z: spring(-0.5) };
+
     const cameraPosition = new Vector3(-20, 11, 30);
     const cameraPositionX = spring(cameraPosition.x);
     const cameraPositionY = spring(cameraPosition.y);
@@ -41,189 +45,154 @@
     });
 
     function setPosition() {
-            switch (selectedCube) {
-                case cube.backend:
-                    backendPosition.x.set(-0.5);
-                    frontendPosition.x.set(20);
-                    databasePosition.x.set(40);
-                    toolPosition.x.set(60);
-                    break;
-                case cube.frontend:
-                    backendPosition.x.set(-20);
-                    frontendPosition.x.set(-0.5);
-                    databasePosition.x.set(20);
-                    toolPosition.x.set(40);
-                    break;
-                case cube.database:
-                    backendPosition.x.set(-40);
-                    frontendPosition.x.set(-20);
-                    databasePosition.x.set(-0.5);
-                    toolPosition.x.set(20);
-                    break;
-                case cube.tool:
-                    backendPosition.x.set(-60);
-                    frontendPosition.x.set(-40);
-                    databasePosition.x.set(-20);
-                    toolPosition.x.set(-0.5);
-                    break;
-            }
+        switch (selectedCube) {
+            case cube.backend:
+                backendPosition.x.set(-0.5);
+                frontendPosition.x.set(20);
+                databasePosition.x.set(40);
+                toolPosition.x.set(60);
+                break;
+            case cube.frontend:
+                backendPosition.x.set(-20);
+                frontendPosition.x.set(-0.5);
+                databasePosition.x.set(20);
+                toolPosition.x.set(40);
+                break;
+            case cube.database:
+                backendPosition.x.set(-40);
+                frontendPosition.x.set(-20);
+                databasePosition.x.set(-0.5);
+                toolPosition.x.set(20);
+                break;
+            case cube.tool:
+                backendPosition.x.set(-60);
+                frontendPosition.x.set(-40);
+                databasePosition.x.set(-20);
+                toolPosition.x.set(-0.5);
+                break;
+        }
     }
-    
 </script>
 
 <T.Group rotation.y={0}>
     <T.PerspectiveCamera
-            makeDefault
-            position={[$cameraPositionX, $cameraPositionY, $cameraPositionZ]}
-            fov={15}
-            on:create={({ ref }) => {
-                ref.lookAt($lookAtX, $lookAtY, $lookAtZ);
-            }}
-            focalLength={10}
+        makeDefault
+        position={[$cameraPositionX, $cameraPositionY, $cameraPositionZ]}
+        fov={15}
+        on:create={({ ref }) => {
+            ref.lookAt($lookAtX, $lookAtY, $lookAtZ);
+        }}
+        focalLength={10}
     >
         <!--    maxPolarAngle={1.4}-->
-        <OrbitControls enableDamping  enablePan={false} maxPolarAngle={1.4} target={[$lookAtX, $lookAtY, $lookAtZ]} maxDistance={50} minDistance={20} 
-            maxAzimuthAngle={2} minAzimuthAngle={-2}    
+        <OrbitControls
+            enableDamping
+            enablePan={false}
+            maxPolarAngle={1.4}
+            target={[$lookAtX, $lookAtY, $lookAtZ]}
+            maxDistance={50}
+            minDistance={20}
+            maxAzimuthAngle={2}
+            minAzimuthAngle={-2}
         />
     </T.PerspectiveCamera>
 </T.Group>
 
-<T.PointLight 
-    position={[1, 3, 10]}
-    intensity={4}
-    decay={0.2}
-    castShadow
-/>
+<T.PointLight position={[1, 3, 10]} intensity={4} decay={0.2} castShadow />
 
 <T.AmbientLight intensity={0.4} />
 
 <T.Group>
-
     <T.Mesh
         position={[-5, -3, 5]}
         scale={0.45}
-
         on:click={(event) => {
             if (selectedCube > 0) {
                 selectedCube -= 1;
-            }
-            else {
+            } else {
                 selectedCube = 3;
             }
             setPosition();
             event.stopPropagation();
         }}
     >
-    
         <T.CylinderGeometry args={[2.5, 2.5, 1.5, 40]} />
-        <T.MeshStandardMaterial color={"#ff0000"} transparent opacity={0}/>
-
+        <T.MeshStandardMaterial color={"#ff0000"} transparent opacity={0} />
     </T.Mesh>
-    
+
     <T.Mesh
-
-            position.x={-5}
-            position.y={-3}
-            position.z={5}
-
-            scale={0.4}
-
-            receiveShadow>
-
+        position.x={-5}
+        position.y={-3}
+        position.z={5}
+        scale={0.4}
+        receiveShadow
+    >
         <T.CylinderGeometry args={[2, 2.5, 1, 40]} />
         <T.MeshStandardMaterial color={"#ffffff"} />
     </T.Mesh>
 
     <T.Mesh
-            rotation.y={30 / 180 * Math.PI}
-            position.x={-5}
-            position.y={-2.75}
-            position.z={5}
-
-            scale={0.3}
-
-            receiveShadow>
-
+        rotation.y={(30 / 180) * Math.PI}
+        position.x={-5}
+        position.y={-2.75}
+        position.z={5}
+        scale={0.3}
+        receiveShadow
     >
+        >
 
-        
         <T.CylinderGeometry args={[2.5, 2.5, 0.5, 3]} />
         <T.MeshStandardMaterial color={"#000000"} />
-
     </T.Mesh>
 
     <T.Mesh
-
-            position.x={5}
-            position.y={-3}
-            position.z={5}
-
-            scale={0.4}
-
-            receiveShadow>
-
+        position.x={5}
+        position.y={-3}
+        position.z={5}
+        scale={0.4}
+        receiveShadow
+    >
         <T.CylinderGeometry args={[2, 2.5, 1, 40]} />
         <T.MeshStandardMaterial color={"#ffffff"} />
     </T.Mesh>
-    
+
     <T.Mesh
-            rotation.y={(30 + 180) / 180 * Math.PI}
-            position.x={5}
-            position.y={-2.75}
-            position.z={5}
-
-            scale={0.3}
-
-            receiveShadow>
-
+        rotation.y={((30 + 180) / 180) * Math.PI}
+        position.x={5}
+        position.y={-2.75}
+        position.z={5}
+        scale={0.3}
+        receiveShadow
     >
+        >
 
-        
         <T.CylinderGeometry args={[2.5, 2.5, 0.5, 3]} />
         <T.MeshStandardMaterial color={"#000000"} />
-
     </T.Mesh>
-    
+
     <T.Mesh
         position={[5, -3, 5]}
         scale={0.45}
-
         on:click={(event) => {
             // change selected cube
             if (selectedCube < 3) {
                 selectedCube += 1;
-            }
-            else {
+            } else {
                 selectedCube = 0;
             }
             setPosition();
             event.stopPropagation();
         }}
     >
-    
         <T.CylinderGeometry args={[2.5, 2.5, 1.5, 40]} />
-        <T.MeshStandardMaterial color={"#ff0000"} transparent opacity={0}/>
-
+        <T.MeshStandardMaterial color={"#ff0000"} transparent opacity={0} />
     </T.Mesh>
 
-    <BackendCube 
-            color={"#00D000"}
-            position={backendPosition}
-        />
+    <BackendCube color={"#00D000"} position={backendPosition} />
 
-    <FrontendCube
-            color={"#F00000"}
-            position={frontendPosition}
-        />
+    <FrontendCube color={"#F00000"} position={frontendPosition} />
 
-    <ToolCube
-            color={"#FA8000"}
-            position={toolPosition}
-        />
+    <ToolCube color={"#FA8000"} position={toolPosition} />
 
-    <DatabaseCube
-            color={"#0059FF"}
-            position={databasePosition}
-        />
-    
+    <DatabaseCube color={"#0059FF"} position={databasePosition} />
 </T.Group>
